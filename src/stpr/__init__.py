bad598595a8413ea42664ebc4658aecbc6922df8
hategan <mhategan@gmail.com>
2026-07-28 14:15:28 -0700

    Updated version to 0.0.2

--- src/stpr/__init__.py
+++ src/stpr/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .transformer import fn, seq, parallel, parallelFor, fork, race, _dump, _fn_id
 from .functions import run, wait, start, exec, async_partial
