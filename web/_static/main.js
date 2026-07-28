08d27d7e1985953a6d841a78b87152e2b2c992f4
hategan <mhategan@gmail.com>
2026-07-28 14:12:03 -0700

    Updated web page

--- web/_static/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        document.getElementById("top_nav").classList.add("shadow");
-    }
-    else {
-        document.getElementById("top_nav").classList.remove("shadow");
-    }
-});
